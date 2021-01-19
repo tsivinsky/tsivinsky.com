b14c433369890f118168766668a608c54ad62e03
Daniil Tsivinsky <daniil@tsivinsky.com>
2021-01-19 12:53:22 +0300

    A little of refactor and styling

--- src/components/SocialLink.svelte
+++ src/components/SocialLink.svelte
@@ -5,19 +5,23 @@
   export let textColor: string;
 
   const isEmail = Boolean(text.startsWith("mailto"));
+
+  const cssVars = `--bg-color: ${bgColor}; --text-color: ${textColor}`;
 </script>
 
 <a
   class="social-link"
-  style="--bg-color: {bgColor}; --text-color: {textColor}"
+  style={cssVars}
   href={url}
   rel={isEmail ? "noopener norefferer" : ""}
-  target="_blank"> {text}</a
->
+  target="_blank">
+  {text}
+</a>
 
 <style lang="scss">
   .social-link {
     width: 70%;
+    margin-bottom: 15px;
     text-decoration: none;
     color: #000000;
     border: 2px solid;
@@ -27,14 +31,15 @@
     font-size: larger;
     cursor: pointer;
     transition: all 0.3s ease;
-    transition-property: background-color, border-color;
+    transition-property: background-color, border-color, box-shadow;
     &:hover {
       background-color: var(--bg-color);
       border-color: transparent;
       color: var(--text-color);
     }
-    &:not(:last-child) {
-      margin-bottom: 15px;
+
+    @media screen and (max-width: 640px) {
+      width: 100%;
     }
   }
 </style>
