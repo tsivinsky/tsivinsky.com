a6321022341a7427281fb6713c1829787a84f7dd
Daniil Tsivinsky <daniil@tsivinsky.com>
2021-01-19 12:28:57 +0300

    Add website files

---
+++ src/App.svelte
@@ -0,0 +1,111 @@
+<script lang="ts">
+  import SocialLink from "./components/SocialLink.svelte";
+
+  interface ISocialLink {
+    text: string;
+    url: string;
+    bgColor: string;
+    textColor: string;
+  }
+
+  const socialLinks: Array<ISocialLink> = [
+    {
+      text: "GitHub",
+      url: "https://github.com/tsivinsky",
+      bgColor: "#24292e",
+      textColor: "#ffffff",
+    },
+    {
+      text: "Twitter",
+      url: "https://twitter.com/daniilcodes",
+      bgColor: "#1DA1F2",
+      textColor: "#ffffff",
+    },
+    {
+      text: "Email",
+      url: "mailto:daniil@tsivinsky.com",
+      bgColor: "#cccccc",
+      textColor: "#ffffff",
+    },
+  ];
+</script>
+
+<div class="container">
+  <div class="inner-block">
+    <img
+      src="me.jpg"
+      alt="It's a me"
+      class="avatar"
+      width="200px"
+      height="200px"
+    />
+    <h1 class="name">Daniil Tsivinsky</h1>
+    <div class="bio">
+      <p>I'm Junior Fullstack Developer based in Russia 🇷🇺.</p>
+      <p>I love coding and playing video games.</p>
+    </div>
+    <div class="social-links">
+      <h2>Social Media</h2>
+      <div class="items">
+        {#each socialLinks as link (link.url)}
+          <SocialLink
+            text={link.text}
+            url={link.url}
+            bgColor={link.bgColor}
+            textColor={link.textColor}
+          />
+        {/each}
+      </div>
+    </div>
+  </div>
+</div>
+
+<style lang="scss">
+  .container {
+    width: 50%;
+    margin: 0 auto;
+    line-height: 2em;
+    .inner-block {
+      display: flex;
+      flex-direction: column;
+      padding-top: 20px;
+      .avatar {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        margin: 0 auto;
+        margin-bottom: 10px;
+        object-fit: cover;
+        border-radius: 50%;
+      }
+      .name {
+        text-align: center;
+      }
+      .bio {
+        text-align: center;
+      }
+      .social-links {
+        width: 60%;
+        margin: 0 auto;
+        h2 {
+          text-align: center;
+        }
+        .items {
+          margin-top: 10px;
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+        }
+      }
+    }
+
+    @media screen and (max-width: 640px) {
+      width: 80%;
+      .inner-block {
+        .social-links {
+          width: 80%;
+        }
+      }
+    }
+  }
+</style>
---
+++ src/components/SocialLink.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+  export let text: string;
+  export let url: string;
+  export let bgColor: string;
+  export let textColor: string;
+
+  const isEmail = Boolean(text.startsWith("mailto"));
+</script>
+
+<a
+  class="social-link"
+  style="--bg-color: {bgColor}; --text-color: {textColor}"
+  href={url}
+  rel={isEmail ? "noopener norefferer" : ""}
+  target="_blank"> {text}</a
+>
+
+<style lang="scss">
+  .social-link {
+    width: 70%;
+    text-decoration: none;
+    color: #000000;
+    border: 2px solid;
+    border-radius: 10px;
+    padding: 12px;
+    text-align: center;
+    font-size: larger;
+    cursor: pointer;
+    transition: all 0.3s ease;
+    transition-property: background-color, border-color;
+    &:hover {
+      background-color: var(--bg-color);
+      border-color: transparent;
+      color: var(--text-color);
+    }
+    &:not(:last-child) {
+      margin-bottom: 15px;
+    }
+  }
+</style>
