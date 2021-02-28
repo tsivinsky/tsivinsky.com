5a587d258b3ca07a42c30c4f51e2ace8ec76a736
Daniil Tsivinsky <daniil@tsivinsky.com>
2021-02-28 19:41:34 +0300

    Add russian version #1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 import "./scss/style.scss";
 
 console.log("Hey 👋");
+
+// Redirect russian users to /ru
+window.addEventListener("load", () => {
+  const lang = navigator.language;
+
+  if (lang === "ru-RU") {
+    window.location.href = "/ru";
+  } else {
+    window.location.href = "/";
+  }
+});
